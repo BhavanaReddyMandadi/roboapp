2b41791e427c0ecd0569734f9a8d4ead06d85b1f
BhavanaMandadi <bhavanmandadi95@gmail.com>
2019-04-12 19:10:58 -0400

    Updates

---
+++ precache-manifest.d6ef76551f2413cddc4329daaa99264c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboapp/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "efc264de6f6c1ba81b70",
+    "url": "/roboapp/static/js/runtime~main.c411ab2e.js"
+  },
+  {
+    "revision": "2c8a148501482132ed34",
+    "url": "/roboapp/static/js/main.ab8c2456.chunk.js"
+  },
+  {
+    "revision": "086e9dc95dd5d5d7088f",
+    "url": "/roboapp/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "2c8a148501482132ed34",
+    "url": "/roboapp/static/css/main.3ee0d317.chunk.css"
+  },
+  {
+    "revision": "086e9dc95dd5d5d7088f",
+    "url": "/roboapp/static/css/2.4d640252.chunk.css"
+  },
+  {
+    "revision": "fdd0762d1eada016211502745dfeedfe",
+    "url": "/roboapp/index.html"
+  }
+];
